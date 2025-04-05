d74f6dd5ec588650d2f284f45a518e19108cff03
didikc <166754124+didikc@users.noreply.github.com>
2025-04-05 22:27:04 +0700

    Update getEpgs.py

--- epg-grabber/getEpgs.py
+++ epg-grabber/getEpgs.py
@@ -76,6 +76,7 @@ urls = [
     'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_PT1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_IT1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_FR1.xml.gz',
 ]
 
 if __name__ == "__main__":
